f9491a8549fd3f359e1021bbbdac1ef8e47a151f
Halim Muhammad <61525711+SSSRankedChad@users.noreply.github.com>
2024-04-05 20:04:42 -0700

    Update portfolio.js

--- projects/Portfolio_Project/portfolio.js
+++ projects/Portfolio_Project/portfolio.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded, function() {
+document.addEventListener('DOMContentLoaded', function() {
     let figure = document.getElementById("portfolio");
     let img = document.getElementById("social");
 });
