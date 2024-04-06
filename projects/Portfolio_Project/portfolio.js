1c2f7389d884a0ca850d8ca4da4a81237d0dde1d
Halim Muhammad <61525711+SSSRankedChad@users.noreply.github.com>
2024-04-05 21:21:35 -0700

    Update portfolio.js

--- projects/Portfolio_Project/portfolio.js
+++ projects/Portfolio_Project/portfolio.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
     let figure = document.getElementById("portfolio");
     let img = document.getElementById("social");
-});
 
 figure.addEventListener("click", function() {
     document.body.style.backgroundColor = "blue";
