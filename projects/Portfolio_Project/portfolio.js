e7e7b652f5420840546b1057c32661bd45d4d9f6
Halim Muhammad <61525711+SSSRankedChad@users.noreply.github.com>
2024-04-07 10:53:45 -0700

    Update portfolio.js

--- projects/Portfolio_Project/portfolio.js
+++ projects/Portfolio_Project/portfolio.js
@@ -3,7 +3,7 @@
     function changeColor(normalColor) {
       for (let i = 0; i < link.length; i++) {
          if (link[i].href) {
-            link[i].style.color = hex;
+            link[i].style.color = normalColor;
          }
       }
     }
